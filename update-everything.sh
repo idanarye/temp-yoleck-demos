ed0084a02ff553f2d212d570fe0614771899f786
Idan Arye <IdanArye@gmail.com>
2022-06-08 12:54:10 +0300

    Also add the level assets...

--- update-everything.sh
+++ update-everything.sh
@@ -1,2 +1,5 @@
 wasm-bindgen ../bevy-yoleck/target/wasm32-unknown-unknown/release/examples/example2d.wasm --out-dir . --target web
 wasm-bindgen ../bevy-yoleck/target/wasm32-unknown-unknown/release/examples/example3d.wasm --out-dir . --target web
+
+cp ../bevy-yoleck/assets/levels2d/example.yol assets/levels2d/
+cp ../bevy-yoleck/assets/levels3d/example.yol assets/levels3d/
